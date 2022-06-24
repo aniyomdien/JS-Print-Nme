d56c9dbd9af31d3bf4683383ec20649ba32b38ac
“[aniyom <[aniyomdien@gmail.com]>
2022-06-24 15:08:46 +0100

    bio data

---
+++ script.js
@@ -0,0 +1,9 @@
+const data = {
+    name: 'Aniyom Dien',
+    height: '5.3 ft',
+    country: 'Nigeria',
+};
+
+alert(
+    `My name is ${data.name} . I am ${data.height} tall and I am from ${data.country}.`
+);
